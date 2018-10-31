dbbc582295dd52361a171c3ad63fef9954a12caf
crypt0s <bryanhalf@gmail.com>
2018-10-31 12:55:36 +0000

    fix issue where wouldn't start with old start line

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 cp /home/pi/.Xauthority /root/.Xauthority
 export DISPLAY=:0.0
 cd /opt/signage/
-nice -20 rpi-fb-matrix --led-brightness=75 --led-daemon --led-slowdown-gpio 4  --led-scan-mode 1 --led-pwm-lsb-nanoseconds 50 resources/matrix.cfg
+nice -20 /opt/rpi-fb-matrix/rpi-fb-matrix --led-brightness=75 --led-daemon --led-slowdown-gpio 4  --led-scan-mode 1 resources/matrix.cfg
 python sign.py
