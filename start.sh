6e9e211416f95086466f2c050e6090c175040d3a
crypt0s <bryanhalf@gmail.com>
2018-12-31 16:37:05 +0000

    fix startup

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 cp /home/pi/.Xauthority /root/.Xauthority
 export DISPLAY=:0.0
 cd /opt/signage/
-nice -20 /opt/rpi-fb-matrix/rpi-fb-matrix --led-brightness=75 --led-daemon --led-slowdown-gpio 4  --led-scan-mode 1 resources/matrix.cfg
+/opt/rpi-fb-matrix/rpi-fb-matrix --led-brightness=75 --led-daemon --led-slowdown-gpio 4  --led-scan-mode 1 resources/matrix.cfg
 python sign.py
